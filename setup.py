064c55ce7f5d329ced8e6b60c2b9ae7990410651
chandan7007 <ckr7007@gmail.com>
2025-12-24 15:59:54 +0530

    project venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Chandan Kumar",
+    author_email="ckr7007@gmail.com",
+    packages=find_packages()
+)
